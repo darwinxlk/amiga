f88c1875421be9ca91f0c46ad64dadcf24d16f69
VINNICI <142064311+Darwinowo@users.noreply.github.com>
2024-10-13 13:00:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡Oh Siii! jajaja mi pami 👉👈';
+    document.getElementById('messageContainer').innerHTML = '¡Oh Siii! jajaja lidani 👉👈';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
